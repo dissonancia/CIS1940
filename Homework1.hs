17154d3dcc24c49e33d4d8eb33e7c63a55d2aedb
Ande <44785864+dissonancia@users.noreply.github.com>
2023-08-25 05:01:18 -0300

    Add files via upload

---
+++ Homework1.hs
@@ -0,0 +1,19 @@
+module Homework1 where
+
+toDigits :: Integer -> [Integer]
+toDigits n
+  | n <= 0 = []
+  | otherwise = map (read . (:[])) $ show n
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev = reverse . toDigits
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = zipWith (*) (cycle l) xs
+  where l = if even $ length xs then [2,1] else [1,2]
+
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . map (sum . toDigits)
+
+validate :: Integer -> Bool
+validate = (==0) . (`mod` 10) . sumDigits . doubleEveryOther . toDigits
